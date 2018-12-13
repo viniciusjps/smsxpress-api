beed2a8016109fb6cab146d28ed4b00f1c0130c2
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-12-13 18:41:57 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Check api');
+  res.send({ status: 'working' });
 });
 
 require('./controllers/smsController')(app);
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log('listening on port ' + port);
+});
