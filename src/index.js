53069690bd3f4f34f256dd57b262ac731665f2da
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-12-15 08:45:33 -0300

    corrigir

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
-}
+});
 
 require('./controllers/smsController')(app);
 
